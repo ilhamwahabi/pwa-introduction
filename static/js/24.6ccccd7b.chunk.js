9cfbdf3b6c3c7830366aa0bfc496dffc00483f86
Ilham Wahabi <wahabiputra@gmail.com>
2018-03-18 18:54:00 +0700

    Updates

--- static/js/24.6ccccd7b.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([24],{985:function(e,a,t){"use strict";Object.defineProperty(a,"__esModule",{value:!0});var r=t(0),i=t.n(r),n=t(428);t.n(n);a.default=i.a.createElement(n.Slide,{bgColor:"secondary"},i.a.createElement(n.Heading,{size:1,fit:!0,caps:!0,lineHeight:1,textColor:"primary"},"Menawan"),i.a.createElement(n.Appear,{order:"1"},i.a.createElement(n.Text,{margin:"0",textColor:"primary",size:3,fit:!0,bold:!0},"Situs web tapi rasa aplikasi native, bukan hybrid, tanpa playstore")))}});
-//# sourceMappingURL=24.6ccccd7b.chunk.js.map
--- static/js/29.38191623.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([29],{980:function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var i=r(0),o=r.n(i),a=r(428);r.n(a);t.default=o.a.createElement(a.Slide,{transition:["zoom"],bgColor:"secondary"},o.a.createElement(a.Heading,{size:1,fit:!0,caps:!0,lineHeight:1,textColor:"primary"},"PWA"),o.a.createElement(a.Text,{margin:"20px 0 0",textColor:"tertiary",size:1,fit:!0,bold:!0},"Progressive Web App - The Introduction"))}});
-//# sourceMappingURL=29.38191623.chunk.js.map
