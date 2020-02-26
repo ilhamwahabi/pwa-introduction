1f8e44fb2c6ddebb7fbc43b6c4a81aab93c49af4
iwgx <wahabiputra@gmail.com>
2020-02-26 21:34:26 +0700

    Update all dependencies

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-## ABOUT
-Presentation about Progressive Web App, new web technology that bring web developer to higher level.
-Made with React.
+## About
 
-## UPDATE
+Web-based presentation about Progressive Web App, new web technology that bring web developer to higher level. Made for Google Developer Student Club chapter ITB workshop.
+
+## Update
+
 Slide will be updated if there are new about PWA.
 
-## USAGE
+## Usage
+
 1. Make sure you had installed node and npm in your computer
 2. Clone this repository
 3. In this directory, run `npm install`
 4. Go for `npm start` and it's live!
 
 ## Future Update
+
 1. Show toast for instruction 'swipe if mobile' and 'arrow if desktop'
 2. Fix mobile screen issues
 3. Add more slide :D
--- src/slides/3section-attention/attention3.js
+++ src/slides/3section-attention/attention3.js
@@ -1,51 +1,44 @@
-import React from 'react'
+import React from "react";
 
-import {
-  Slide,
-  Text,
-  Image,
-  Layout,
-  Fill,
-  Appear
-} from 'spectacle';
+import { Slide, Text, Image, Layout, Fill, Appear } from "spectacle";
 
 export default (
-  <Slide transition={['fade']} bgColor="#e0e0e0">
-    <Appear order="1">        
+  <Slide transition={["fade"]} bgColor="#e0e0e0">
+    <Appear order="1">
       <Text margin="0 0 50px 0" textColor="secondary" size={1} fit bold>
         Apakah harus menggunakan...
       </Text>
     </Appear>
     <Layout>
-      <Appear order="2">      
+      <Appear order="2">
         <Fill>
           <Image src="https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png" />
         </Fill>
       </Appear>
 
-      <Appear order="3">              
+      <Appear order="3">
         <Fill>
           <Image src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/full/angular2.png" />
         </Fill>
       </Appear>
 
-      <Appear order="4">      
+      <Appear order="4">
         <Fill>
-          <Image src="https://genesisui-static-dcklmia32ll.netdna-ssl.com/assets/img/logos/vue.svg" />
+          <Image src="https://vuejs.org/images/logo.png" />
         </Fill>
       </Appear>
-        
-      <Appear order="5">      
+
+      <Appear order="5">
         <Fill>
           <Image src="http://gregfranko.com/images/backbone.png" />
         </Fill>
       </Appear>
 
-      <Appear order="6">      
+      <Appear order="6">
         <Fill>
           <Image src="https://emberjs.com/images/tomster-sm.png" />
         </Fill>
       </Appear>
     </Layout>
   </Slide>
-)
+);
