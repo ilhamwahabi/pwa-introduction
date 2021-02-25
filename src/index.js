ae3fd40d6c5cd885eab755b1f93751c071c639cb
iwgx <wahabiputra@gmail.com>
2021-02-26 05:28:18 +0700

    Install splitbee

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import splitbee from '@splitbee/web';
+
 import Presentation from './presentation';
 import registerServiceWorker from './registerServiceWorker';
+
+splitbee.init()
 
 ReactDOM.render(<Presentation />, document.getElementById('root'));
 registerServiceWorker();
